6468956ca578f162f7a14223a9d802a3a86757a8
Abdullah khan <akhan283785@gmail.com>
2024-11-24 13:23:06 +0530

    first try

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Fields from "./fields";
 
 const App = () => {
+  const eleValue = [
+    {
+      name: "Name",
+      type: "text",
+      id: "name",
+    },
+
+    {
+      name: "Email",
+      type: "email",
+      id: "email",
+    },
+
+    {
+      name: "Password",
+      type: "password",
+      id: "password",
+    },
+  ];
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Fields eleValue={eleValue} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
